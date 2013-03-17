df0b427584480e42e43c6b3234adfa2944bb0a7a
Dan Kogai <dankogai+github@gmail.com>
2013-03-17 11:43:52 +0900

    refactored recursion

--- object-clone.js
+++ object-clone.js
@@ -80,25 +80,27 @@
             : (1 / x === 1 / y) // +-0
         : (x !== x && y !== y); // NaN
     };
-    function isnt(x, y) { return !is(x, y) };
-    function equals(x, y, vx, vy) {
-        if (isPrimitive(x)) return is(x, y);
-        if (isFunction(x))  return is(x, y);
-        // check deeply
-        var sx = signatureOf(x), sy = signatureOf(y);
-        var i, l, px, py, sx, sy, kx, ky, dx, dy, dk;
-        if (sx !== sy) return false;
-        switch (sx) {
-        case '[object Array]':
-        case '[object Object]':
-            if (isExtensible(x) !== isExtensible(y)) return false;
-            if (isSealed(x) !== isSealed(y)) return false;
-            if (isFrozen(x) !== isFrozen(y)) return false;
-            if (HASWEAKMAP) {
-                if (!vx) {
-                    vx = WeakMap();
-                    vy = WeakMap();
-                } else {
+    function isnt (x, y) { return !is(x, y) };
+    function equals (x, y) {
+        var vx, vy;
+        if (HASWEAKMAP) {
+            vx = WeakMap();
+            vy = WeakMap();
+        }
+        return (function _equals(x, y) {
+            if (isPrimitive(x)) return is(x, y);
+            if (isFunction(x))  return is(x, y);
+            // check deeply
+            var sx = signatureOf(x), sy = signatureOf(y);
+            var i, l, px, py, sx, sy, kx, ky, dx, dy, dk;
+            if (sx !== sy) return false;
+            switch (sx) {
+            case '[object Array]':
+            case '[object Object]':
+                if (isExtensible(x) !== isExtensible(y)) return false;
+                if (isSealed(x) !== isSealed(y)) return false;
+                if (isFrozen(x) !== isFrozen(y)) return false;
+                if (vx) {
                     if (vx.has(x)) {
                         // console.log('circular ref found');
                         return vy.has(y);
@@ -106,80 +108,82 @@
                     vx.set(x, true);
                     vy.set(y, true);
                 }
-            };
-            px = getOwnPropertyNames(x),
-            py = getOwnPropertyNames(y);
-            if (px.length != py.length) return false;
-            px.sort(); py.sort();
-            iter: for (i = 0, l = px.length; i < l; ++i) {
-                kx = px[i];
-                ky = py[i];
-                if (kx !== ky) return false;
-                dx = getOwnPropertyDescriptor(x, ky);
-                dy = getOwnPropertyDescriptor(y, ky);
-                if (!equals(dx.value, dy.value, vx, vy)) return false
-                delete dx.value;
-                delete dy.value;
-                for (dk in dx) {
-                    if (!is(dx[dk], dy[dk])) return false;
+                px = getOwnPropertyNames(x),
+                py = getOwnPropertyNames(y);
+                if (px.length != py.length) return false;
+                px.sort(); py.sort();
+                iter: for (i = 0, l = px.length; i < l; ++i) {
+                    kx = px[i];
+                    ky = py[i];
+                    if (kx !== ky) return false;
+                    dx = getOwnPropertyDescriptor(x, ky);
+                    dy = getOwnPropertyDescriptor(y, ky);
+                    if (!_equals(dx.value, dy.value)) return false
+                    delete dx.value;
+                    delete dy.value;
+                    for (dk in dx) {
+                        if (!is(dx[dk], dy[dk])) return false;
+                    }
                 }
+                return true;
+            case '[object RegExp]':
+            case '[object Date]':
+            case '[object String]':
+            case '[object Number]':
+            case '[object Boolean]':
+                return ''+x === ''+y;
+            default:
+                throw TypeError(sx + ' not supported');
             }
-            return true;
-        case '[object RegExp]':
-        case '[object Date]':
-        case '[object String]':
-        case '[object Number]':
-        case '[object Boolean]':
-            return ''+x === ''+y;
-        default:
-            throw TypeError(sx + ' not supported');
+        })(x, y);
+    }
+    function clone(src, deep) {
+        var wm;
+        if (deep && HASWEAKMAP) {
+            wm = WeakMap();
         }
-    };
-    function clone(src, deep, wm) {
-        // primitives and functions
-        if (isPrimitive(src)) return src;
-        if (isFunction(src)) return src;
-        var sig = signatureOf(src);
-        switch (sig) {
-        case '[object Array]':
-        case '[object Object]':
-            if (deep && HASWEAKMAP) {
-                if (!wm) {
-                    wm = WeakMap();
-                } else {
+        return (function _clone(src) {
+            // primitives and functions
+            if (isPrimitive(src)) return src;
+            if (isFunction(src)) return src;
+            var sig = signatureOf(src);
+            switch (sig) {
+            case '[object Array]':
+            case '[object Object]':
+                if (wm) {
                     if (wm.has(src)) {
                         // console.log('circular ref found');
                         return src;
                     }
                     wm.set(src, true);
                 }
-            };
-            var isarray = isArray(src);
-            var dst = isarray ? [] : create(getPrototypeOf(src));
-            getOwnPropertyNames(src).forEach(function(k) {
-                // Firefox forbids defineProperty(obj, 'length' desc)
-                if (isarray && k === 'length') {
-                    dst.length = src.length;
-                } else {
-                    var desc = getOwnPropertyDescriptor(src, k);
-                    if (deep && 'value' in desc) 
-                        desc.value = clone(src[k], deep, wm);
-                    defineProperty(dst, k, desc);
-                }
-            });
-            if (!isExtensible(src)) preventExtensions(dst);
-            if (isSealed(src)) seal(dst);
-            if (isFrozen(src)) freeze(dst);
-            return dst;
-        case '[object RegExp]':
-        case '[object Date]':
-        case '[object String]':
-        case '[object Number]':
-        case '[object Boolean]':
-            return deep ? new src.constructor(src.valueOf()) : src;
-        default:
-            throw TypeError(sig + ' is not supported');
-        }
+                var isarray = isArray(src);
+                var dst = isarray ? [] : create(getPrototypeOf(src));
+                getOwnPropertyNames(src).forEach(function(k) {
+                    // Firefox forbids defineProperty(obj, 'length' desc)
+                    if (isarray && k === 'length') {
+                        dst.length = src.length;
+                    } else {
+                        var desc = getOwnPropertyDescriptor(src, k);
+                        if (deep && 'value' in desc) 
+                            desc.value = _clone(src[k]);
+                        defineProperty(dst, k, desc);
+                    }
+                });
+                if (!isExtensible(src)) preventExtensions(dst);
+                if (isSealed(src)) seal(dst);
+                if (isFrozen(src)) freeze(dst);
+                return dst;
+            case '[object RegExp]':
+            case '[object Date]':
+            case '[object String]':
+            case '[object Number]':
+            case '[object Boolean]':
+                return deep ? new src.constructor(src.valueOf()) : src;
+            default:
+                throw TypeError(sig + ' is not supported');
+            }
+        })(src);
     };
     // Object
     defaults(Object, defSpecs({
